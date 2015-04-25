ff31e2603630071873a11c9fd8cef5890be279c9
Evan Turner <evturn@gmail.com>
2015-04-25 18:48:00 -0400

    using Cherrio to load html with request

--- server.js
+++ server.js
@@ -3,3 +3,23 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
+
+app.get('/scrape', function(req, res) {
+	url = 'http://www.imdb.com/title/tt1229340/';
+
+	request(url, function(error, response, html) {
+		if(!error) {
+
+			var $ = cheerio.load(html);
+
+			var title, release, rating;
+			var json = {title : "", release : "", rating : ""};
+
+
+		}
+	})
+});
+
+app.listen('8081')
+console.log('Express listening on port 8081');
+exports = module.exports = app;
